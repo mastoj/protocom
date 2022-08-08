35ca463ce050c9351a9ddd676cd5ca065ce11238
Tomas Jansson <tomas.jansson@gmail.com>
2022-08-08 11:41:22 +0200

    Add db for product

--- src/ProtoCom.App/Grains/ProductGrain.cs
+++ src/ProtoCom.App/Grains/ProductGrain.cs
@@ -9,14 +9,17 @@ public class ProductGrain : ProductGrainBase
     private static Random _random = new();
     private IDocumentStore _documentStore;
 
-    private static ConcurrentDictionary<string, Product> _productDb = 
-        new(Enumerable.Range(0, 100)
-            .ToDictionary(i => i.ToString(), i => new Product { Id = i.ToString(), Name = $"Product {i}", Price = 100 + i }));
+    // private static ConcurrentDictionary<string, Product> _productDb = 
+    //     new(Enumerable.Range(0, 100)
+    //         .ToDictionary(i => i.ToString(), i => new Product { Id = i.ToString(), Name = $"Product {i}", Price = 100 + i }));
+    private Product? _product;
 
     public ProductGrain(IContext context, ClusterIdentity clusterIdentity, IDocumentStore documentStore) : base(context)
     {
         Console.WriteLine("===> Started product grain: " + clusterIdentity.Identity);
         _documentStore = documentStore;
+        // var session = documentStore.LightweightSession();
+        // _product = session.Query<Product>().FirstOrDefault(p => p.Id == clusterIdentity.Identity);
         // Product = MissingProduct;
     }
 
@@ -34,9 +37,10 @@ public class ProductGrain : ProductGrainBase
 
     public override async Task AddProduct(AddProductRequest request)
     {
-        _productDb.AddOrUpdate(request.Product.Id, request.Product, (key, oldValue) => request.Product);
+        // _productDb.AddOrUpdate(request.Product.Id, request.Product, (key, oldValue) => request.Product);
         using var session = _documentStore.LightweightSession();
         session.Store(request.Product);
+        _product = request.Product;
         await session.SaveChangesAsync();
         // return Task.CompletedTask;
     }
@@ -53,11 +57,15 @@ public class ProductGrain : ProductGrainBase
             throw new Exception("KILL");
             // Context.Stop(Context.Self);
         // var product = _productDb!.GetValueOrDefault(request.ProductId, null);
-        using var session = _documentStore.LightweightSession();
-        var product = session.Query<Product>().FirstOrDefault(p => p.Id == request.ProductId);
+        // using var session = _documentStore.LightweightSession();
+        // var product = session.Query<Product>().FirstOrDefault(p => p.Id == request.ProductId);
+        if(_product is null) {
+            var session = _documentStore.LightweightSession();
+            _product = session.Query<Product>().FirstOrDefault(p => p.Id == request.ProductId);
+        }
         return Task.FromResult(new GetProductResponse() {
-            Product = product,
-            ProductStatus = product is null ? ProductStatus.MissingProduct : ProductStatus.Success
+            Product = _product,
+            ProductStatus = _product is null ? ProductStatus.MissingProduct : ProductStatus.Success
         });
     }
 }
